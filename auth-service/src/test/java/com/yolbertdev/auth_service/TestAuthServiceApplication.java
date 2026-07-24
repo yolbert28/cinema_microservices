a8d0f5d33b7784ebd949780f5a438998c7ef7ec0
yolbert28 <yolbertdev@gmail.com>
2026-07-24 14:26:11 -0400

    first commit

---
+++ auth-service/src/test/java/com/yolbertdev/auth_service/TestAuthServiceApplication.java
@@ -0,0 +1,11 @@
+package com.yolbertdev.auth_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AuthServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
